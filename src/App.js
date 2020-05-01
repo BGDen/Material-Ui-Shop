cb1f5e8a94bd408d96f03cba154296370cff8e21
Den <fentau.line@gmail.com>
2020-05-02 00:21:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import 'typeface-roboto';
-import {SimpleContainer} from './components/main-container'
-// import {AppHeader} from './components/header';
-// import {AddButton} from './components/add-button';
+import {SimpleContainer} from './components/main-container';
 
 export class App extends Component {
 
   render(){
     return (
-      // <div>
+      
         <SimpleContainer />
-        /* <AppHeader />
         
-        <AddButton edge="end"/> */
-      // </div>    
     );
   }
 }
